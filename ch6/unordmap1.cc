2d74b6cb7b96ee8840e4ef5e2cf14a957884a41a
HuaTsai <a0910666734@gmail.com>
2018-09-18 18:17:44 +0800

    6.2.3

---
+++ ch6/unordmap1.cc
@@ -0,0 +1,16 @@
+#include <unordered_map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+  unordered_map<string, double> coll {{"tim", 9.9}, {"struppi", 11.77}};
+
+  for (pair<const string, double> &elem : coll) {
+    elem.second *= elem.second;
+  }
+
+  for (const auto &elem : coll) {
+    cout << elem.first << ": " << elem.second << endl;
+  }
+}
