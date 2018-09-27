0b7afbe39d5887ab51f63965ed50e8a7d78be0eb
HuaTsai <a0910666734@gmail.com>
2018-09-28 02:51:29 +0800

    6.7.3

---
+++ ch6/remove4.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <list>
+using namespace std;
+
+int main() {
+  list<int> coll;
+
+  for (int i = 1; i <= 6; ++i) {
+    coll.push_front(i);
+    coll.push_back(i);
+  }
+
+  // bad
+  coll.erase(remove(coll.begin(), coll.end(), 3), coll.end());
+
+  // good
+  coll.remove(4);
+}
