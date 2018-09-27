23bedf1a58545792a13da7311469e72c5d02f3c1
HuaTsai <a0910666734@gmail.com>
2018-09-28 02:55:04 +0800

    6.10.1

---
+++ ch6/foreach2.cc
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class PrintInt {
+ public:
+  void operator()(int elem) const {cout << elem << ' ';}
+};
+
+int main() {
+  vector<int> coll;
+
+  for (int i = 1; i <= 9; ++i) {
+    coll.push_back(i);
+  }
+
+  for_each(coll.cbegin(), coll.cend(), PrintInt());
+  cout << endl;
+}
