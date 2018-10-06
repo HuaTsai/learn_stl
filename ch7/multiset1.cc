6d8c0fea75fe911b561e1e42e77e5b734d143b9c
HuaTsai <a0910666734@gmail.com>
2018-10-07 02:32:57 +0800

    7.7.4

--- ch7/multiset1.cc
+++ ch7/multiset1.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  set<int, greater<int>> coll1;
+  multiset<int, greater<int>> coll1;
   coll1.insert({4, 3, 5, 1, 6, 2});
   coll1.insert(5);
 
@@ -14,15 +14,11 @@ int main() {
   }
   cout << endl;
 
-  auto status = coll1.insert(4);
-  if (status.second) {
-    cout << "4 inserted as element "
-         << distance(coll1.begin(), status.first) + 1 << endl;
-  } else {
-    cout << "4 already exists" << endl;
-  }
+  auto ipos = coll1.insert(4);
+  cout << "4 inserted as element "
+       << distance(coll1.begin(), ipos) + 1 << endl;
 
-  set<int> coll2(coll1.cbegin(), coll1.cend());
+  multiset<int> coll2(coll1.cbegin(), coll1.cend());
   copy(coll2.cbegin(), coll2.cend(), ostream_iterator<int>(cout, " "));
   cout << endl;
 
